557cf9427249209a84741881c53535c1199c3f45
codeleaded <gnu.c.reaper@gmail.com>
2026-05-03 22:10:28 +0200

    update

--- src/Client.c
+++ src/Client.c
@@ -4,23 +4,23 @@
 
 #define SIGNAL_MSG  (SIGNAL_START + 0)
 
-void Client_Proc_Connect(Vector* signalhandlers,Client* c,void* data,int size){
+void Client_Proc_Connect(void* parent,Signal s,Client* c,void* data,PackageSize size){
     printf("Client_Connect: %d\n",c->sockfd);
     Client_Signal_Send(c,SIGNAL_CONNECT,NULL,0);
 }
-void Client_Proc_Disconnect(Vector* signalhandlers,Client* c,void* data,int size){
+void Client_Proc_Disconnect(void* parent,Signal s,Client* c,void* data,PackageSize size){
     printf("Client_Disconnect: %d\n",c->sockfd);
     Client_Signal_Send(c,SIGNAL_DISCONNECT,NULL,0);
 }
-void Client_Proc_Msg(Vector* signalhandlers,Client* c,void* data,int size){
+void Client_Proc_Msg(void* parent,Signal s,Client* c,void* data,PackageSize size){
     printf("Client_Msg: %d -> '%s' (%d)\n",c->sockfd,(char*)data,size);
 }
 
 int main(){
     Client s = Client_Make("5000","192.168.2.99",(SignalHandler[]){
-        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,void*,void*,int))Client_Proc_Connect),
-        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,void*,void*,int))Client_Proc_Disconnect),
-        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,void*,void*,int))Client_Proc_Msg),
+        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,Signal,void*,void*,PackageSize))Client_Proc_Connect),
+        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,Signal,void*,void*,PackageSize))Client_Proc_Disconnect),
+        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,Signal,void*,void*,PackageSize))Client_Proc_Msg),
         SignalHandler_Null()
     });
 
--- src/Server.c
+++ src/Server.c
@@ -4,21 +4,21 @@
 
 #define SIGNAL_MSG  (SIGNAL_START + 0)
 
-void Server_Proc_Connect(Vector* signalhandlers,Client* c,void* data,int size){
+void Server_Proc_Connect(void* parent,Signal s,Client* c,void* data,PackageSize size){
     printf("Server_Connect: %d\n",c->sockfd);
 }
-void Server_Proc_Disconnect(Vector* signalhandlers,Client* c,void* data,int size){
+void Server_Proc_Disconnect(void* parent,Signal s,Client* c,void* data,PackageSize size){
     printf("Server_Disconnect: %d\n",c->sockfd);
 }
-void Server_Proc_Msg(Vector* signalhandlers,Client* c,void* data,int size){
+void Server_Proc_Msg(void* parent,Signal s,Client* c,void* data,PackageSize size){
     printf("Server_Msg: %d -> '%s' (%d)\n",c->sockfd,(char*)data,size);
 }
 
 int main(){
     Server s = Server_Make(5000,10,(SignalHandler[]){
-        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,void*,void*,int))Server_Proc_Connect),
-        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,void*,void*,int))Server_Proc_Disconnect),
-        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,void*,void*,int))Server_Proc_Msg),
+        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,Signal,void*,void*,PackageSize))Server_Proc_Connect),
+        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,Signal,void*,void*,PackageSize))Server_Proc_Disconnect),
+        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,Signal,void*,void*,PackageSize))Server_Proc_Msg),
         SignalHandler_Null()
     });
 
