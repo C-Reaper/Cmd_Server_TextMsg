b2c8518b3f892e5e51e5556d4012776c5ffd4ea5
codeleaded <gnu.c.reaper@gmail.com>
2026-07-21 02:56:47 +0200

    updated

--- src/Client.c
+++ src/Client.c
@@ -1,43 +1,55 @@
 #include "/home/codeleaded/System/Static/Library/CStr.h"
-#include "/home/codeleaded/System/Static/Library/Networking.h"
+#include "/home/codeleaded/System/Static/Library/Networking_Event.h"
 
 
-#define SIGNAL_MSG  (SIGNAL_START + 0)
+#define NET_EVENT_MSG  (NET_EVENT_START + 0)
 
-void Client_Proc_Connect(void* parent,Signal s,Client* c,void* data,PackageSize size){
-    printf("Client_Connect: %d\n",c->sockfd);
-    Client_Signal_Send(c,SIGNAL_CONNECT,NULL,0);
+void Net_EventClient_Proc_Connect(void* parent,Net_EventClient* c,Net_EventPackage* p){
+    printf("[Net_EventServer]: Connect -> %d\n",(uint32_t)Net_Client_Id(&c->client));
+    Net_EventClient_Send(c,NET_EVENT_CONNECT,NULL,0);
 }
-void Client_Proc_Disconnect(void* parent,Signal s,Client* c,void* data,PackageSize size){
-    printf("Client_Disconnect: %d\n",c->sockfd);
-    Client_Signal_Send(c,SIGNAL_DISCONNECT,NULL,0);
+void Net_EventClient_Proc_Disconnect(void* parent,Net_EventClient* c,Net_EventPackage* p){
+    printf("[Net_EventServer]: Disconnect -> %d\n",(uint32_t)Net_Client_Id(&c->client));
+    Net_EventClient_Send(c,NET_EVENT_DISCONNECT,NULL,0);
 }
-void Client_Proc_Msg(void* parent,Signal s,Client* c,void* data,PackageSize size){
-    printf("Client_Msg: %d -> '%s' (%d)\n",c->sockfd,(char*)data,size);
+void Net_EventClient_Proc_Msg(void* parent,Net_EventClient* c,Net_EventPackage* p){
+    printf("[Net_EventServer]: Msg -> %u: '%s' (%u)\n",(uint32_t)Net_Client_Id(&c->client),(char*)p->pack.data,(uint32_t)p->pack.size);
+}
+
+char running = 1;
+void* Net_EventClient_Recver(Net_EventClient* c){
+    while(running){
+        Net_EventClient_Update(c);
+        Net_EventClient_DoAll(c,NULL);
+        Thread_Sleep_M(NET_SERVER_DELAY);
+    }
+    return NULL;
 }
 
 int main(){
-    Client s = Client_Make("5000","192.168.2.99",(SignalHandler[]){
-        SignalHandler_New(SIGNAL_CONNECT,(void (*)(void*,Signal,void*,void*,PackageSize))Client_Proc_Connect),
-        SignalHandler_New(SIGNAL_DISCONNECT,(void (*)(void*,Signal,void*,void*,PackageSize))Client_Proc_Disconnect),
-        SignalHandler_New(SIGNAL_MSG,(void (*)(void*,Signal,void*,void*,PackageSize))Client_Proc_Msg),
-        SignalHandler_Null()
+    Net_EventClient s = Net_EventClient_Make("5000","192.168.2.99",(Net_EventHandler[]){
+        Net_EventHandler_New(NET_EVENT_CONNECT,     Net_EventClient_Proc_Connect),
+        Net_EventHandler_New(NET_EVENT_DISCONNECT,  Net_EventClient_Proc_Disconnect),
+        Net_EventHandler_New(NET_EVENT_MSG,         Net_EventClient_Proc_Msg),
+        Net_EventHandler_Null()
     });
 
-    while(1){
+    Thread recver = Thread_New(NULL,(void*(*)(void*))Net_EventClient_Recver,&s);
+    Thread_Start(&recver);
+
+    while(running){
         char buffer[1024];
         int size = scanf("%1023s",buffer);
-        if(CStr_Cmp(buffer,"exit")) break;
         
-        Client_Signal_Send(&s,SIGNAL_MSG,buffer,CStr_Size(buffer) + 1);
-
-        Client_Update(&s);
-        Client_DoAll(&s,NULL);
-
-        printf("Waiting...\n");
-        Thread_Sleep_M(1000);
+        if(CStr_Cmp(buffer,"exit")){
+            running = 0;
+        }else{
+            Net_EventClient_Send(&s,NET_EVENT_MSG,buffer,CStr_Size(buffer) + 1);
+            Thread_Sleep_M(100);
+        }
     }
     
-    Client_Free(&s);
+    Thread_Join(&recver);
+    Net_EventClient_Free(&s);
     return 0;
 }
